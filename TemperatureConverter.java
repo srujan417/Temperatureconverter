e8e853b76c73aa424f7be496b955fd83179e2912
srujan417 <113100280+srujan417@users.noreply.github.com>
2023-05-16 12:10:34 +0530

    Update TemperatureConverter.java

--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class temperature_con {
+public class Temperatureconverter{
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
